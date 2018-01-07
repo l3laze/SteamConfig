ea7cad1e31f2b992652204d2c7f6ac7fb0563570
Tom Shaver <l3l_aze@yahoo.com>
2018-01-07 04:40:16 -0600

    Better error handling
    As long as I'm understanding nolanlawson somewhat correctly in https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html and https://pouchdb.com/2015/03/05/taming-the-async-beast-with-es7.html

--- examples/log_info.js
+++ examples/log_info.js
@@ -6,28 +6,31 @@ let steam = new SteamConfig()
 
 async function run () {
   try {
-    steam.setInstallPath('/Users/tmshvr/Library/Application Support/Steam')
+    steam.setInstallPath(steam.detectPath())
     await steam.loadRegistryLM()
     await steam.loadLoginusers()
+    steam.setUser()
+    await steam.loadLibraryfolders()
+    await steam.loadSteamapps()
     await steam.loadConfig()
     await steam.loadLibraryfolders()
     await steam.loadSteamapps()
     await steam.loadAppinfo()
-    // await steam.loadPackageinfo();
-
-    steam.setUser()
+    // await steam.loadPackageinfo()
 
     if (steam.user === null) {
-      console.error(`Error: No user associated with the Steam installation @ ${steam.loc}`)
-      process.exit(1)
+      throw new Error(`There is no user associated with the Steam installation @ ${steam.loc}`)
     }
 
     await steam.loadSharedconfig()
     await steam.loadLocalconfig()
     await steam.loadShortcuts()
   } catch (err) {
-    console.error(err.message)
-    console.error(err.stack)
+    if ((err.message.indexOf('Failed to load ') !== -1 || err.message.indexOf('Failed to save ')) && err.message.indexOf(' because ') !== -1) {
+      console.error(err.message)
+    } else {
+      console.error(err)
+    }
     process.exit(1)
   }
 
@@ -38,6 +41,7 @@ run()
 
 function logData () {
   console.info(`Install location:\t${steam.loc}`)
+  console.info(`Active user:\t${steam.registry.Registry.HKCU.Software.Valve.Steam.AutoLoginUser}`)
   console.info(`Users:\t\t\t${Object.keys(steam.loginusers.users).length}`)
   console.info(`Current User:\t\t${steam.user.PersonaName} (${steam.user.AccountName})`)
   console.info(`Library Folders:\t${steam.nondefaultLibraryfolders.length + 1}`)
@@ -46,3 +50,9 @@ function logData () {
   console.info(`Non-Steam:\t\t${Object.keys(steam.shortcuts.shortcuts).length}`)
   console.info(`Appinfo Entries:\t${steam.appinfo.length}`)
 }
+
+process.on('uncaughtException', (err) => {
+  console.error(err)
+  console.error(err.captureStackTrace())
+  process.exit(1)
+})
